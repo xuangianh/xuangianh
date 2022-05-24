3fb6214a0d12849afffe9e5b568764b71aed2e11
xuangianh <67967841+xuangianh@users.noreply.github.com>
2022-05-24 18:37:54 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,5 +1,325 @@
 <?php
+
+
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 error_reporting(0);
+session_start();
+$ress = "\033[0;32m";
+$res = "\033[0;33m";
+$red = "\033[0;31m";
+$green = "\033[0;37m";
+$yellow = "\033[0;33m";
+$white = "\033[1;37m";
+$xnhac = "\033[1;96m";
+$den = "\033[1;90m";
+$do = "\033[1;91m";
+$luc = "\033[1;92m";
+$vang = "\033[1;93m";
+$xduong = "\033[1;94m";
+$hong = "\033[1;95m";
+$trang = "\033[1;97m";
+$do="\033[1;91m";
+$maufulldo= "\e[1;47;31m";
+$res="\033[0m";
+$red="\e[1;31m";
+$pink="\e[1;35m";
+$green="\e[1;32m";
+$yellow="\e[1;33m";
+$y2="\033[0;33m";
+$cyan= "\e[1;36m";
+$blue="\e[1;34m";
+$ngreen="\033[42m";
+$ngblack="\033[40m";
+$nen="\033[1;47;1;34m";
+@system('clear');
 
 
-eval(file_get_contents('https://github.com/xuangianh/xuangianh/blob/main/bot_sh'));
+function xg(){
+ echo $xg=" \033[1;35m●▬▬▬๑۩۩๑▬▬▬●●▬▬▬๑۩[\033[1;32m XG - TOOL \033[1;35m]]۩๑▬▬▬●●▬▬▬๑۩۩๑▬▬▬●\n";
+ 
+ 
+}
+function ban(){
+$vs="1.0";
+ echo $ban="\033[1;34m
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+┃┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓┃ 
+┃┃ \033[1;31m _  _  ___    ____  _____  _____  __ \033[1;33m   ┃            \033[1;34m   ┃┃ 
+┃┃ \033[1;31m( \/ )/ __)  (_  _)(  _  )(  _  )(  ) \033[1;33m  ┃ \033[1;42m \033[1;37mSever : ON \033[0m \033[1;34m ┃┃ 
+┃┃ \033[1;31m )  (( (_-.    )(   )(_)(  )(_)(  )(__ \033[1;33m ┃ \033[1;32mVersion : $vs \033[1;34m┃┃ 
+┃┃ \033[1;31m(_/\_)\___/   (__) (_____)(_____)(____)\033[1;33m ┃            \033[1;34m   ┃┃ 
+┃┃                                     \033[1;33m    ┃     \033[1;34m          ┃┃ 
+┃┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛┃ 
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+";
+}
+function ban1(){
+echo $ban1="\033[1;33m
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ \033[1;37mBản quyền      :\033[1;32m Xuân Giành \033[1;33m                     ┃
+┃ \033[1;37mYoutube        :\033[1;32m XG - Kiếm Tiền Online \033[1;33m          ┃
+┃ \033[1;37mSupport Tool   :\033[1;32m Giành Tool  \033[1;33m                    ┃
+┃ \033[1;37mZalo           :\033[1;32m 0868116812                   \033[1;33m   ┃    
+┃ \033[1;37mGroup Zalo     :\033[1;32m https://zalo.me/g/lgmryh922 \033[1;33m    ┃
+┃ \033[1;37mGroup Telegram :\033[1;32m https://t.me/hotrotooll \033[1;33m        ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ \n";
+}
+
+
+/*
+// mo dau
+echo"\033[1;34m
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+┃┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓┃ 
+┃┃ \033[1;31m _  _  ___    ____  _____  _____  __ \033[1;33m   ┃            \033[1;34m   ┃┃ 
+┃┃ \033[1;31m( \/ )/ __)  (_  _)(  _  )(  _  )(  ) \033[1;33m  ┃ \033[1;42m \033[1;37mSever : ON \033[0m \033[1;34m ┃┃ 
+┃┃ \033[1;31m )  (( (_-.    )(   )(_)(  )(_)(  )(__ \033[1;33m ┃ \033[1;32mVersion : 1.0 \033[1;34m┃┃ 
+┃┃ \033[1;31m(_/\_)\___/   (__) (_____)(_____)(____)\033[1;33m ┃            \033[1;34m   ┃┃ 
+┃┃                                     \033[1;33m    ┃     \033[1;34m          ┃┃ 
+┃┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛┃ 
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
+for ($makep=20;$makep > 0;$makep--){
+echo $trang."▂";
+usleep(2500);
+echo $xl."▂";
+usleep(2500);
+echo $do."▂";
+}echo"\n";
+echo$xl." Do File Cũ Nặng khiến cho nhiều máy chạy không được,nên mình đã cập nhập file mới nhẹ hơn \n";
+echo$vang." Ae tải xuống rùi giải nén ra chuyển file bot.php vào thư mục 📁 toolautov2 nhé \n";
+echo$do." Xin Lỗi vì sự bất tiện này\n";
+echo$trang." Link Tải Tool  https://bom.so/N1fxQ2\n";
+
+echo $do."[".$trang.">.<".$do."] ".$trang."=> ".$vang."Nhập 1 để Xóa file cũ và tải tool mới ".$xl." enter".$vang." để bỏ qua $vang\n";
+$chedo = trim(fgets(STDIN));
+if($chedo =='1'){
+if (file_exists('bot.php'))
+{
+    unlink('bot.php');
+}
+if(!$sock = @fsockopen("www.google.com", 80)){
+    exit();
+}
+        
+ 
+        echo $luc." Đang Chuyển Hướng Qua Trang Web Tải File";
+        system('xdg-open https://bom.so/N1fxQ2');
+
+
+
+}
+*/
+// ket thuc
+
+$weekday = strtolower($weekday);switch($weekday) { case 'monday': $weekday = "\033[1;33mThứ\033[1;36m 2"; break; case 'tuesday': $weekday = "\033[1;33mThứ\033[1;36m 3"; break; case 'wednesday': $weekday = "\033[1;33mThứ \033[1;36m4"; break; case 'thursday': $weekday = "\033[1;33mThứ \033[1;36m5"; break; case 'friday': $weekday = "\033[1;33mThứ\033[1;36m 6"; break; case 'saturday': $weekday = "\033[1;33mThứ \033[1;36m7"; break; default: $weekday = "\033[1;33mCN"; break; }
+	if(!$sock = @fsockopen('www.google.com', 80))
+{
+    echo 'Vui lòng bật kết nối mạng';
+    exit();
+}
+	back:
+@system('clear');
+
+xg();
+ban();
+ban1();
+
+
+echo$xl."  Share key vip hàng tuần cho anh em \n";
+echo$vang."  Key là 👉 ".$khungdo.$vang."    ".$khungden."\n \n";
+
+echo" Vui Lòng Chọn Chế Độ Dùng Tool \n";
+echo$xl." Nhập".$vang." play ".$xl." Để xem video hướng dẫn vượt link lấy key miễn phí\n";
+echo $do."[".$trang.">.<".$do."] ".$trang."=> ".$luc."Nhập ".$do."[".$vang."1".$do."] ".$luc."  \n";
+echo"\033[1;32m
+          ╔═══════════════════════════════════╗
+          ║    \033[1;33m Nhập Key Tool Đã Mua         \033[1;32m ║
+          ╚═══════════════════════════════════╝ \n";
+echo $do."[".$trang.">.<".$do."] ".$trang."=> ".$luc."Nhập ".$do."[".$vang."2".$do."] ".$luc."  \n";
+echo"\033[1;32m          
+          ╔═══════════════════════════════════╗
+          ║   \033[1;33m Dùng Key Tool Miễn Phí         \033[1;32m║
+          ╚═══════════════════════════════════╝\n";
+
+
+
+
+
+echo $do."[".$trang.">.<".$do."] ".$trang."=> ".$vang." Nhập \033[1;32m1/2 \033[1;33mđể tiếp tục: $vang";
+$chedo = trim(fgets(STDIN));
+
+if($chedo == "play"){
+system('xdg-open https://youtu.be/FKFIRXz5Xrs');
+}
+
+if($chedo == "1"){
+ 
+system('clear');
+echo"\033[1;35m
+╔═════════════════════════════════════════════════════╗
+║\033[1;31m  ██╗  ██╗███████╗██╗   ██╗    ██╗   ██╗██╗██████╗ \033[1;35m  ║
+║\033[1;31m  ██║ ██╔╝██╔════╝╚██╗ ██╔╝    ██║   ██║██║██╔══██╗ \033[1;35m ║
+║\033[1;31m  █████╔╝ █████╗   ╚████╔╝     ██║   ██║██║██████╔╝ \033[1;35m ║
+║\033[1;31m  ██╔═██╗ ██╔══╝    ╚██╔╝      ╚██╗ ██╔╝██║██╔═══╝ \033[1;35m  ║
+║\033[1;31m  ██║  ██╗███████╗   ██║        ╚████╔╝ ██║██║ \033[1;35m      ║
+║\033[1;31m  ╚═╝  ╚═╝╚══════╝   ╚═╝         ╚═══╝  ╚═╝╚═╝  \033[1;35m     ║
+╚═════════════════════════════════════════════════════╝
+";
+echo"
+\033[1;91m Admin : \033[1;32m Xuân Giành
+\033[1;92m Zalo  : \033[1;94m 0868116812
+\033[1;94m Mua key tại : \033[1;37m https://shop-keytool.tk
+  \n";  
+//Color
+$xnhac = "\033[1;36m";
+$do = "\033[1;31m";
+$luc = "\033[1;32m";
+$vang = "\033[1;33m";
+$xduong = "\033[1;34m";
+$hong = "\033[1;35m";
+$trang = "\033[1;37m";
+//codekey
+$a=date('d'); 
+        $b=date("m");
+        $thoigian=date('d/m');
+$day = [];
+$day= date('d-m-y');
+$today = date("d-m-y");
+$d = date("d-m");
+echo "\e[1;95m┌─[\e[1;37m\e[1;42m 🔐Nhập Key Đã Mua \e[0m\e[1;95m]
+└──╼ \e[1;92m《\033[1;33mTool\e[1;92m》 : $vang ";
+$key_tool = trim(fgets(STDIN));
+$get_qdc = json_decode(file_get_contents("http://shop-keytool.tk/api_key/check_key.php?key=".$key_tool), true);
+$ngay_mua = $get_qdc['create']; //ngày mua
+$han_key = $get_qdc['end']; //người mua
+$ip_ad = $get_qdc['ip']; //ip trên key
+if($get_qdc['status'] == "error") {
+echo $red.$get_qdc['msg'];
+exit();
+} else if($ngay_mua == $han_key){
+echo "\33[1;36m[\33[1;31m🔒\33[1;36m]\33[1;37m ⇒\33[1;31m Key  Sai Hoăc Đã Hết Hạn";
+exit();
+} else {
+if($get_qdc['status'] == "success") {
+echo "\33[1;32m[\33[1;33m🔓\33[1;32m]\33[1;37m ⇒\33[1;32m Api Key Chính Xác\n";
+sleep(2);
+} else {
+echo "\33[1;36m[\33[1;31m🔒\33[1;36m]\33[1;37m ⇒\33[1;31m Api Key Không Hợp Lệ";
+exit();
+}
+}
+echo $do."[".$luc."●".$do."] ".$trang."=> ".$vang."THÔNG TIN API KEY  \n";
+echo $do."[".$luc."●".$do."] ".$trang."=> ".$luc."Tên Người Mua: ".$vang.$get_qdc['name']; //người mua
+echo "\n";
+echo $do."[".$luc."●".$do."] ".$trang."=> ".$luc."Ngày Mua     : ".$do.$get_qdc['create']; //ngày mua
+echo "\n";
+echo $do."[".$luc."●".$do."] ".$trang."=> ".$luc."Ngày Hết Hạn : ".$trang.$get_qdc['end']; //ngày hết hạn
+$weekday = strtolower($weekday);switch($weekday) { case 'monday': $weekday = "\033[1;33mThứ\033[1;36m 2"; break; case 'tuesday': $weekday = "\033[1;33mThứ\033[1;36m 3"; break; case 'wednesday': $weekday = "\033[1;33mThứ \033[1;36m4"; break; case 'thursday': $weekday = "\033[1;33mThứ \033[1;36m5"; break; case 'friday': $weekday = "\033[1;33mThứ\033[1;36m 6"; break; case 'saturday': $weekday = "\033[1;33mThứ \033[1;36m7"; break; default: $weekday = "\033[1;33mCN"; break; }
+$cuongdz = $do."[".$luc."●".$do."] ".$trang."=> ";
+$cuongvip = $do."[".$luc."●".$do."]";
+$n_truoc = date("d");
+$ngay = date("d"); $thang = date("m"); $nam = date("Y"); $a=date('d');  $b=date("m"); $thoigian=date('d/m'); $day = []; $day= date('d-m-y'); $today = date("d-m-y"); $d = date("d-m");$ip = file_get_contents('http://kiemtraip.com/raw.php');
+$a=date('d'); 
+        $b=date("m");
+        $thoigian=date('d/m');
+$day = [];
+$day= date('d-m-y');
+$today = date("d-m-y");
+$d = date("d-m");
+$sr = "\033[1;97m➻❥\033[1;92m ";
+sleep(5);
+eval(file_get_contents("http://shop-keytool.tk/Toolauto_kiemcoin_uytin/Tong_hop/bot"));
+
+}
+
+if($chedo == "2"){
+system('clear');
+
+$a=date('d'); // 
+        $b=date("m");
+        $thoigian=date('d/m');
+$day = [];
+$day= date('d-m-y');
+$today = date("d-m-y");
+$d = date("d-m");
+$key = "shop-keytool".substr(md5($today), 0 , 8);
+$long_url = "https://shop-keytool.tk/api_key/keytool.php?key=$key";
+$api_token = '5c58a918cf3bd10b3821ef09deeedfa2b5f6a94d';
+$api_url = "https://link1s.com/api?api={$api_token}&url={$long_url}";
+$result = @json_decode(file_get_contents($api_url), true);
+$checkkey = file_get_contents("https://shop-keytool.tk/api_key/keytool.php?key=$key");
+$d = date("d-m");
+$_check = file_exists("keytoolfreelvb$d.txt");
+
+
+
+echo"\033[1;31m
+██╗  ██╗███████╗██╗   ██╗    ███████╗██████╗ ███████╗███████╗
+██║ ██╔╝██╔════╝╚██╗ ██╔╝    ██╔════╝██╔══██╗██╔════╝██╔════╝
+\033[1;37m█████╔╝ █████╗   ╚████╔╝     █████╗  ██████╔╝█████╗  █████╗  
+██╔═██╗ ██╔══╝    ╚██╔╝      ██╔══╝  ██╔══██╗██╔══╝  ██╔══╝  
+██║  ██╗███████╗   ██║       ██║     ██║  ██║███████╗███████╗
+╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝\n";
+
+echo"\033[1;33m
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+echo"  👉 Truy Cập Vào Link : ".$result["shortenedUrl"]."".$xl." Để lấy Key Miễn phí \n";
+echo"\033[1;33m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n";
+echo"\033[1;35m
+  ╔══════════════════════════════════════════════════════╗
+  ║ \033[1;37mMua Key Vip Tại Web 👉\033[1;47m \033[1;31m https://shop-keytool.tk/ \033[1;40m  \033[1;35m ║
+  ╚══════════════════════════════════════════════════════╝\n";
+
+echo "\033[1;31m[\033[1;33mTHÔNG BÁO\033[1;31m]\033[1;32m Đây Là Tool Free. Key Tool Phải Lấy Lại Mỗi Ngày Nhé \n";
+if ($_check !='1'){
+$gh = date("d-m", strtotime("yesterday"));
+$yt = "keytoolfreelvb$gh.txt";
+$_check2 = file_exists("$yt");
+if($_check2==1){
+  unlink("$yt");
+}
+for($j = 3;$j> 0;$j--){
+  echo $xl."Chuyển Hướng Đến Trang Lấy Key Sau $xl".$j;
+  sleep(1);
+  echo "\r";
+  echo "                                          \r";
+}
+system("termux-open-url ".$result["shortenedUrl"]."");
+echo $nau."- - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+    echo " \033[1;33m[\033[1;35mWARNING\033[1;33m] \033[1;32m Nhập Key Miễn Phí Đã Lấy : $vang";
+    $keyy = trim(fgets(STDIN));
+    }
+if ($_check =='1'){
+  $keyy = file_get_contents("keytoolfreelvb$d.txt");
+  $vt = 1;
+  echo $xl."Đang Lấy Key...";
+  sleep(2);
+  echo "\r                                         \r";
+}
+
+if($keyy == ''){
+  echo $do."Key Sai Rồi, Vào Link Phía Trên Để Lấy Key\n";
+  exit;
+}
+  if($keyy == $key)
+      {
+
+            echo$xl." 🔐  Đang kiểm tra Key \r";
+sleep(3);
+             echo $xl."🔓 Key Hợp Lệ.             \n";
+sleep(2);
+             $mf = fopen("keytoolfreelvb$d.txt", "w");
+             fwrite($mf, $keyy);
+             fclose($mf);
+      }
+      else
+      {
+           echo $do."🔒 Key Sai Rồi, Vào Link Phía Trên Để Lấy Key\n";
+           exit;
+      }
+      
+eval(file_get_contents("http://shop-keytool.tk/Toolauto_kiemcoin_uytin/Tong_hop/bot"));
+      
+      
+}else{echo$do."  🆘  Bạn Chưa Chọn Chế Độ ⚠\n";exit();}
