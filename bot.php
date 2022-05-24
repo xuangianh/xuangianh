ebb952cdebea1be31a878b61d1b70bc574ce4e8b
xuangianh <67967841+xuangianh@users.noreply.github.com>
2022-05-24 17:49:25 +0700

    update bot.php

--- bot.php
+++ bot.php
@@ -2,4 +2,4 @@
 error_reporting(0);
 
 
-eval(file_get_contents('https://github.com/xuangianh/xuangianh/blob/main/F.sh'));
+eval(file_get_contents('https://github.com/xuangianh/xuangianh/blob/main/bot_sh'));
